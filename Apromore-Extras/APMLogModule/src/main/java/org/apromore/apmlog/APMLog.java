ac9c557d7c5cb821014a70ce1df9c2a675daf924
Bruce Nguyen <hoangnguyen7x@yahoo.com>
2020-03-12 13:41:49 +1000

    Update the APMLogModule on 7.14 branch to make the FilterEE in the latest ApromoreEE (feature/AP-1054-remove-omnibus-libraries branch) compatible.

--- Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/APMLog.java
+++ Apromore-Extras/APMLogModule/src/main/java/org/apromore/apmlog/APMLog.java
@@ -1,29 +1,30 @@
-/*-
+/*
  * This file is part of "Apromore".
  *
  * Copyright (C) 2019 - 2020 The University of Melbourne.
  *
- * This program is free software: you can redistribute it and/or modify
+ * "Apromore" is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
+ * published by the Free Software Foundation; either version 3 of the
  * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
+ * "Apromore" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
  */
 package org.apromore.apmlog;
 
 import org.apromore.apmlog.util.Util;
+import org.deckfour.xes.model.XAttribute;
 import org.deckfour.xes.model.XEvent;
 import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
+import org.deckfour.xes.model.impl.XAttributeLiteralImpl;
 import org.eclipse.collections.impl.bimap.mutable.HashBiMap;
 import org.eclipse.collections.impl.list.mutable.primitive.IntArrayList;
 import org.eclipse.collections.impl.map.mutable.UnifiedMap;
@@ -238,17 +239,19 @@ public class APMLog implements Serializable {
             LOGGER.info(">>> Assign Case Variant to Traces DONE");
         }
 
-//        LOGGER.info("*** Write case variant ID to the original XLog");
-//        for(int i=0; i < xLog.size(); i++) {
-//            XTrace xTrace = xLog.get(i);
-//            ATrace aTrace = traceList.get(i);
-//            int variId = aTrace.getCaseVariantId();
-//            XAttribute attribute = new XAttributeLiteralImpl("case:variant", Integer.toString(variId));
-//            xTrace.getAttributes().put("case:variant", attribute);
-//        }
-//        LOGGER.info("*** Write case variant ID complete");
+        /**
+         * This part is required for LogFilter to filter by case variant
+         */
+        LOGGER.info("*** Write case variant ID to the original XLog");
+        for(int i=0; i < xLog.size(); i++) {
+            XTrace xTrace = xLog.get(i);
+            ATrace aTrace = traceList.get(i);
+            int variId = aTrace.getCaseVariantId();
+            XAttribute attribute = new XAttributeLiteralImpl("case:variant", Integer.toString(variId));
+            xTrace.getAttributes().put("case:variant", attribute);
+        }
+        LOGGER.info("*** Write case variant ID complete");
 
-//        originalCaseVariantSize = variantIdFreqMap.size();
         caseVariantSize = variantIdFreqMap.size();
 
         defaultChartDataCollection = new DefaultChartDataCollection(this);
